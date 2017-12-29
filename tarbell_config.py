fe1784d2a13396f6f99c4638f08654ae4e91c34d
ryanbmarx <ryanbmarx@gmail.com>
2017-12-29 15:02:57 -0600

    These filters will help generate various UI elements to reflect the assorted categories of projects

--- tarbell_config.py
+++ tarbell_config.py
@@ -4,8 +4,50 @@
 Tarbell project configuration
 """
 
+from flask import Blueprint, g, render_template
+# import ftfy
+from markupsafe import Markup
+
+import jinja2
+
+# For list flattening in get tag list
+import operator
+
+blueprint = Blueprint('ryan-portfolio', __name__)
+
+# @blueprint.app_template_global('get_labels')
+# @blueprint.app_template_filter()
+# @jinja2.contextfilter
+@blueprint.app_template_filter('get_tags')
+def get_tags(tags, return_html=False):
+    if return_html:
+        retval = ""
+        for t in tags.split(','):
+            retval += "<li>#{}</li>".format(t.strip())
+        # return Markup(retval)
+    else:
+        retval = ""
+        for t in tags.split(','):
+            retval += " data-{}".format(t.strip())
+    return Markup(retval)
+
+@blueprint.app_template_global('get_tags_list')
+def get_tags_list(projects):
+    """
+    takes the entire projects sheet and creates a unique list of tags. This 
+    would be great to make filter buttons from. Just sayin'
+    """
+    tag_list = []
+    for p in projects:
+        tag_list.append(p['tags'].split(','))
+    
+    tag_list = reduce(operator.add, tag_list)
+    tag_list = list(set(tag_list))
+    return tag_list
+	
+
 # Google spreadsheet key
-#SPREADSHEET_KEY = "None"
+SPREADSHEET_KEY = "16I7B3l2Dew220S_NwCQjTMCnTK9eCbjjlxpbEmA2fGU"
 
 # Exclude these files from publication
 EXCLUDES = ['*.md', 'requirements.txt', 'node_modules', 'sass', 'js/src', 'package.json', 'Gruntfile.js', 'subtemplates']

13b0590248bb04457f56161c6fa6cc8c3b8c02fa
ryanbmarx <ryanbmarx@gmail.com>
2017-12-29 15:16:19 -0600

    Stub out some JS things.

--- js/src/app.js
+++ js/src/app.js
@@ -1,1 +1,22 @@
+import fontawesome from '@fortawesome/fontawesome';
+import { faFacebook } from '@fortawesome/fontawesome-free-solid';
+
 // TODO: Add smoothscroll polyfill
+
+
+
+
+
+
+
+window.addEventListener('DOMContentLoaded', function(e){
+	// Init the portfolio filter buttons
+	const filterButtons = [].slice.call(document.querySelectorAll('.filter'));
+	filterButtons.forEach(f => {
+		f.addEventListener('click', function(e){
+			console.log('click');
+			if (this.classList.contains('filter--active')) this.classList.remove('filter--active');
+			else this.classList.add('filter--active');
+		})
+	})
+})

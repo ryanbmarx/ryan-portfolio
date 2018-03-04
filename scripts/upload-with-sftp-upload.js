db8c8afb1bdcbad8e0d356e92c15ef9ece8bf14b
ryanbmarx <ryanbmarx@gmail.com>
2018-03-03 18:03:36 -0600

    Since tarbell only publishes to S3, I need a way to publish/upload changes to my webserver. I found a handy npm module which basically does just that. I generate my site, login via SSH and upload away.

---
+++ scripts/upload-with-sftp-upload.js
@@ -0,0 +1,25 @@
+const 	SftpUpload = require('sftp-upload'),
+		fs = require('fs'),
+		remotePath="//var/www/ryanbmarx.com/public_html/",
+		localPath="/Users/ryanbmarx/Code/ryan-portfolio",
+		directoryToUpload = process.argv[2],
+		serverConfig = {
+			host: '174.138.74.153', 
+			port: 22,
+			path: directoryToUpload,
+			remoteDir: remotePath,
+			username:"ryanbmarx",
+			privateKey: fs.readFileSync('/Users/ryanbmarx/.ssh/id_rsa') 
+		},
+		sftp = new SftpUpload(serverConfig);
+
+
+sftp.on('error', err => console.log(err))
+    .on('uploading', function(progress) {
+        console.log('Uploading', progress.file);
+        console.log(progress.percent+'% completed');
+    })
+    .on('completed', function() {
+        console.log('Upload Completed');
+    })
+    .upload();
